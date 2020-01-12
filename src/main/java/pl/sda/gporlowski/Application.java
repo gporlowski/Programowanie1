7a19aa3b6a87cb755d7153acb30975768f7e4dff
Grzegorz Orlowski <g.p.orlowski@gmail.com>
2020-01-12 11:20:35 +0100

    Dodanie System out

--- src/main/java/pl/sda/gporlowski/Application.java
+++ src/main/java/pl/sda/gporlowski/Application.java
@@ -4,11 +4,13 @@ import static pl.sda.gporlowski.UnitConverter.*;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println();
         System.out.println(celsiusToFahrenheit(30));
         System.out.println(fahrenheitToCelsius(50));
         System.out.println(kilometerToMile(20));
         System.out.println(mileToKilometer(20));
         System.out.println(kilogramToPound(5));
         System.out.println(poundToKilogram(5));
+        System.out.println();
     }
 }
