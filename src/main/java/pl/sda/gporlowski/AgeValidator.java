b101d4c99b19d80bc40bd4751c54a87c9bf9c120
Grzegorz Orlowski <g.p.orlowski@gmail.com>
2020-01-18 11:31:46 +0100

    Expand the AgeValidator class

--- src/main/java/pl/sda/gporlowski/AgeValidator.java
+++ src/main/java/pl/sda/gporlowski/AgeValidator.java
@@ -7,6 +7,34 @@ public class AgeValidator {
     public static boolean  isAdult(Person person){
         LocalDate today = LocalDate.now();
         LocalDate ageLimit = today.minusYears(18);
-        return person.getDateOfBirth().isBefore(ageLimit) || person.getDateOfBirth().equals(ageLimit);
+
+        if (person.getDateOfBirth().isAfter(ageLimit)){
+            System.out.println("Access denied");
+            return false;
+        }
+
+        int yearFromPesel = Integer.parseInt(person.getPeselNumber().substring(0, 2));
+        int monthFromPesel = Integer.parseInt(person.getPeselNumber().substring(2,4));
+        int dayFromPesel = Integer.parseInt(person.getPeselNumber().substring(4,6));
+
+        if (Integer.parseInt(person.getPeselNumber().substring(2, 3)) > 1){
+            yearFromPesel += 2000;
+            monthFromPesel -= 20;
+        } else {
+            yearFromPesel += 1900;
+        }
+
+        LocalDate dateFromPesel = LocalDate.of(yearFromPesel, monthFromPesel, dayFromPesel);
+
+        if (!dateFromPesel.isEqual(person.getDateOfBirth())) {
+            System.out.println("Access denied");
+            System.out.println("Error. The date of birth is incorrect.");
+            return false;
+        }
+
+        System.out.println("Acces granted.");
+        return true;
     }
+
+
 }
--- src/main/java/pl/sda/gporlowski/Application.java
+++ src/main/java/pl/sda/gporlowski/Application.java
@@ -8,7 +8,7 @@ public class Application {
     public static void main(String[] args) {
 
         Person person1 = new Person("Jan", "Brzechwa", "98081559355", LocalDate.of(1998,8,15));
-        Person person2 = new Person ("Janek", "Brzechwa", "02281577178", LocalDate.of(2010, 8, 15));
+        Person person2 = new Person ("Janek", "Brzechwa", "02281577178", LocalDate.of(2002, 8, 15));
 
         System.out.println(AgeValidator.isAdult(person1));
         System.out.println(AgeValidator.isAdult(person2));
