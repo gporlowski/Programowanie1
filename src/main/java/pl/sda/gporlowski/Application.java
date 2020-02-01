01b8e143fe37a8e294182bcc95cbb7a07ef782d0
Grzegorz Orlowski <g.p.orlowski@gmail.com>
2020-02-01 09:18:32 +0100

    Code update

--- src/main/java/pl/sda/gporlowski/Application.java
+++ src/main/java/pl/sda/gporlowski/Application.java
@@ -1,34 +1,10 @@
 package pl.sda.gporlowski;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-
-        MyLinkedList list = new MyLinkedList();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-
-        list.printAll();
-
-        /*String first = "Hi, I'm first";
-        String second = "Hi, I'm second";
-        String third = "Hi, I'm third";
-        StackImpl stack = new StackImpl();
-
-        stack.push(first);
-        stack.push(second);
-        stack.push(third);
-
-        Object peekResult = stack.peek();
-        System.out.println((String) peekResult);
-
-        Object popResult = stack.pop();
-        System.out.println((String) popResult);
-
-        Object popResultTwo = stack.pop();
-        System.out.println((String) popResultTwo);*/
-
     }
 }
--- src/main/java/pl/sda/gporlowski/Person.java
+++ src/main/java/pl/sda/gporlowski/Person.java
@@ -33,4 +33,20 @@ public class Person {
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPeselNumber(String peselNumber) {
+        this.peselNumber = peselNumber;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
